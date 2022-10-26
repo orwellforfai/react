2de4bca8050fa357cb358cc264d97dd91b4e7408
Martin Beltramo <94710842+orwellforfai@users.noreply.github.com>
2022-10-25 22:57:46 -0300

    Commit inicial

--- src/components/NavBar/navBar.jsx
+++ src/components/NavBar/navBar.jsx
@@ -5,9 +5,9 @@ const NavBar = () => {
     return (
         <ul>
             <li><a href="#home">Home</a></li>
-            <li><a href="#news">News</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="#news">Noticias</a></li>
+            <li><a href="#contact">Contactenos</a></li>
+            <li><a href="#about">Nosotros</a></li>
         </ul>
 
     )
