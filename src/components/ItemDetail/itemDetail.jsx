f58c3d7a1bb256721e758f9a453419777b202b26
Martin Beltramo <94710842+orwellforfai@users.noreply.github.com>
2022-11-15 18:47:49 -0300

    Go Cart

--- src/components/ItemDetail/itemDetail.jsx
+++ src/components/ItemDetail/itemDetail.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./itemDetail.css"
 import ItemCount from "../ItemCount/itemCount";
 
 const ItemDetail = ({character}) => {
 
+    const [quantityItemDetal, setQuantityItemDetail] = useState(0)
+
     const confirmPurchase = (quantity) => {
         console.log(quantity)
+        setQuantityItemDetail(quantity)
     }
 
     return (
@@ -13,12 +16,15 @@ const ItemDetail = ({character}) => {
             <img src={character.img} width={450}/>
             <h1>{character.name}</h1>
             <button className='btn btn-danger sm-3'>Comprar Poster</button>
-            <ItemCount
+            {quantityItemDetal ?
+                <button> Go to CART </button>
+                :
+                            <ItemCount
                 onAdd={confirmPurchase}
                 initial={1}
                 stock={100}
                 />
-
+            }
 
         </div>
     )
