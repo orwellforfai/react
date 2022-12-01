4211781a89bb899d65466dbbd1ff819bb952ec51
Martin Beltramo <94710842+orwellforfai@users.noreply.github.com>
2022-12-01 13:41:29 -0300

    formulario con error

--- src/containers/CartContainer/cartContainer.jsx
+++ src/containers/CartContainer/cartContainer.jsx
@@ -2,25 +2,24 @@ import React, {useContext} from 'react';
 import {Shop} from "../../contexts/shop";
 import CartItem from "../../components/CartItem/cartItem";
 import Form from "../../components/Form/form";
-
-
+import {useNavigate} from "react-router-dom";
 
 
 const CartContainer = () => {
     const {products} = useContext(Shop)
 
-    const confirmPurchase = () => {
-        <Form/>
-    }
+    const navigate = useNavigate()
 
+    const handleNavigate = () => {
+        navigate('/form')
+    }
 
     return (
         <div>
             {products.map(product => {
-                return <CartItem key={product.id} item={product} />
+                return <CartItem key={product.id} item={product}/>
             })}
-            <button className='btn btn-danger sm-3' Link={confirmPurchase()} > Confirma Compra? </button>
-            <Form/>
+            <button className='btn btn-danger sm-3' onClick={handleNavigate}> Confirma Compra?</button>
         </div>
     );
 };
