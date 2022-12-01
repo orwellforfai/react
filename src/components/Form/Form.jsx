95d4eb764ffebcd7ea8f8e228e2d985fee95df67
Martin Beltramo <94710842+orwellforfai@users.noreply.github.com>
2022-12-01 18:54:15 -0300

    Creacion de ordenes en firebase con error

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { useFormik } from 'formik';
+import { collection, addDoc } from "firebase/firestore";
+import { db } from '../../firebase/config';
+import generateOrderObject from "../.../services/generateOrderObject";
 
 const Form = () => {
-
 
     const formik = useFormik({
         initialValues: {
@@ -15,9 +17,28 @@ const Form = () => {
             alert(JSON.stringify(values, null, 2));
         }
     })
+
+    async ()=> {
+        const firstname = firstname
+        const lastname = lastname
+        const email = email
+            total: totalCart()
+            date: new Date().toLocaleDateString()
+
+        const generatedOrder = generateOrderObject(firstname,lastname,email,products,totalCart())
+        console.log(generatedOrder);
+    }
+    try {
+        const docRef = await addDoc(collection(db, "orders"), generatedOrder);
+        alert(`Se generó la order correctamente con ID: ${docRef.id}`)
+    } catch (error) {
+        console.log(error)
+    }
+    }
+
     return (
 
-        <div>
+        <div >
             <form onSubmit={formik.handleSubmit}>
                 <label htmlFor="firstName">First Name</label>
                 <input id="firstName" name="firstName" placeholder="Diego" onChange={formik.handleChange} value={formik.values.firstName}
@@ -41,7 +62,7 @@ const Form = () => {
                 <input id="total" name="total" placeholder="total" onChange={formik.handleChange} value={formik.values.total}
                 />
 
-                <button type="submit">Submit</button>
+                <button type="submit" onClick={  confirmPurchase(orderData)}>Submit</button>
             </form>
 
         </div>
